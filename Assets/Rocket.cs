a3909d7611c2d8f6c666614486c60c87dfb42d4d
TechnodromeLtd <junttila.eero@gmail.com>
2018-12-31 05:07:16 +0200

    level1 created, added light

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -9,45 +9,74 @@ public class Rocket : MonoBehaviour
 
     Rigidbody rigidbody;
     AudioSource audioSource;
+    [SerializeField] float thrustSpeed = 100; 
+    [SerializeField] float rotationSpeed = 100; 
    // Rigidbody[] thrusters;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+
         
+
        // thrusters = GetComponentsInChildren<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      ProcessInput();
+        Thrust();
+        Rotate();
+     
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
-       if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-
-
-            rigidbody.AddRelativeForce(Vector3.up*100*Time.deltaTime);
+            float thrustThisFrame = thrustSpeed * Time.deltaTime * 10;
+            rigidbody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!audioSource.isPlaying)
                 audioSource.Play();
-           // thrusters[0].AddRelativeForce(Vector3.up*100*Time.deltaTime);
+           
 
-        } 
-       else
+        }
+        else
         {
             audioSource.Stop();
         }
+    }
 
-       if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
+    private void Rotate()
+    {
+
+        rigidbody.freezeRotation = true;
+        float rotationThisFrame = rotationSpeed * Time.deltaTime ;
+       if (Input.GetKey(KeyCode.LeftArrow))
+        { 
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
-       else if (Input.GetKey(KeyCode.D))
+       else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rigidbody.freezeRotation = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("OK");
+                break;
+            case "Fuel":
+                Debug.Log("Fuel");
+                break;
+            default:
+                Debug.Log("Dead");
+                break;
+
         }
     }
 }
