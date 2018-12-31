bb59f7bbc12a3136f6327bc44c82521ba6e5442d
TechnodromeLtd <junttila.eero@gmail.com>
2018-12-31 04:14:36 +0200

    Added controls and engine sound

---
+++ Assets/Rocket.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+   // Rigidbody[] thrusters;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
+       // thrusters = GetComponentsInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+       if(Input.GetKey(KeyCode.Space))
+        {
+
+
+            rigidbody.AddRelativeForce(Vector3.up*100*Time.deltaTime);
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+           // thrusters[0].AddRelativeForce(Vector3.up*100*Time.deltaTime);
+
+        } 
+       else
+        {
+            audioSource.Stop();
+        }
+
+       if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+       else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
